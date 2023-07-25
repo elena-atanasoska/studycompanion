6209f0ca4e9d883e74ba56c52f591d1aafaeceee
elena-atanasoska <elena.atanasoska@students.finki.ukim.mk>
2023-07-25 14:11:42 +0200

    index page

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import *
+from django.contrib.auth.admin import UserAdmin
+
+# class CustomUserAdmin(UserAdmin):
+#     list_display = ('username', 'first_name', 'last_name', 'is_staff', 'is_active')
+
+# Register your custom user model with the custom admin class
+admin.site.register(CustomUser)
+admin.site.register(Comment)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import Comment
+
+
+def your_view(request):
+    comments = Comment.objects.all()
+    return render(request, 'index.html', {'comments': comments})
+
+
